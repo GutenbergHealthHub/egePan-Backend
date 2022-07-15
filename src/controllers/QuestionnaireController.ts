d2ea381fb63a1ee5a4ff3991389c14ea07c1c61f
mbastian93 <m_bastian@outlook.de>
2022-07-15 09:54:00 +0200

    add endpoint to get questionnaire by url & version

--- src/controllers/QuestionnaireController.ts
+++ src/controllers/QuestionnaireController.ts
@@ -7,9 +7,11 @@
 import { Request, Response } from 'express';
 
 import { Controller, Get, Middleware } from '@overnightjs/core';
+import { expressjwt as jwt } from 'express-jwt';
 
 import { QuestionnaireModel } from '../models/QuestionnaireModel';
 import { AuthorizationController } from './AuthorizationController';
+import { AuthConfig } from '../config/AuthConfig';
 
 /**
  * Endpoint class for all questionnaire related restful methods.
@@ -48,6 +50,56 @@ export class QuestionnaireController {
                 } else {
                     res.status(500).end();
                 }
+            }
+        );
+    }
+
+    /**
+     * Update a questionnaire.
+     *
+     * @param {Request} req
+     * @param {Response} res
+     * @memberof QuestionnaireController
+     */
+    @Get('')
+    @Middleware(
+        jwt({
+            secret: AuthConfig.jwtSecret,
+            algorithms: ['HS256'],
+            requestProperty: 'payload',
+            isRevoked: AuthorizationController.checkApiUserLogin
+        })
+    )
+    public async getQuestionnaireByUrlAndVersion(req: Request, res: Response) {
+        let url: string, version: string;
+        try {
+            url = req.query.url.toString();
+            version = req.query.version.toString();
+        } catch (err) {
+            res.status(400).json({
+                errorCode: 'InvalidQuery',
+                errMessage: `Query failed with error: '${err.message}'.`,
+                errorStack: process.env.NODE_ENV !== 'production' ? err.stack : undefined
+            });
+            return;
+        }
+
+        this.questionnaireModel.getQuestionnaireByUrlAndVersion(url, version).then(
+            (response) => {
+                if (response.length === 0) {
+                    res.status(404).json({
+                        errorCode: 'QuestionnaireNotFound',
+                        errorMessage: 'No questionnaire found that matches the given parameters.'
+                    });
+                }
+                res.status(200).json(response[0]);
+            },
+            (err) => {
+                res.status(500).json({
+                    errorCode: 'Internal error',
+                    errorMessage: 'Query failed.',
+                    errorStack: process.env.NODE_ENV !== 'production' ? err.stack : undefined
+                });
             }
         );
     }
